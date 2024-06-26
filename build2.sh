50ad4e16410d559a74516f4dff5d004633b162de
yzchnb <1021777674@qq.com>
2024-06-26 19:59:29 +0800

    fix error

---
+++ build2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker_user=10.1.75.10:30003/library/admin
+image_name=jupyterlab-management-frontend
+image_tag=v3
+
+
+image=${docker_user}/${image_name}:${image_tag}
+echo "Target docker image: ${image}"
+
+# Build the project
+echo "Step 1: Building the project..."
+npm run build
+
+# Build the docker image
+echo "Step 2: Building the docker image: ${docker_user}/${image_name}:${image_tag}"
+docker build -t ${docker_user}/${image_name}:${image_tag} .
+
+# Push the docker image
+echo "Step 3: Pushing the docker image..."
+docker push ${docker_user}/${image_name}:${image_tag}
